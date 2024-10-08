feaaa9ad8e46831c870c74a508ffd3cdb54bb2d0
HayHay <dev@hayhay.dev>
2024-10-08 14:46:15 -0400

    Update cache service

--- src/utils/cacheService.ts
+++ src/utils/cacheService.ts
@@ -1,32 +1,54 @@
 import { Context } from "hono";
 
+const QUALIFIED_CACHE_URL = "https://api.questkeeper.app/v1/";
+
 export async function getCachedResponse(
-  c: Context<{}, any, {}>,
   cache: Cache,
   categoryId: string,
   itemId: string
 ): Promise<any> {
-  const cacheKey = `${c.req.url}::${categoryId}-${itemId}`;
+  const cacheKey = `${QUALIFIED_CACHE_URL}::${categoryId}-${itemId}`;
   const cachedResponse = await cache.match(cacheKey); // Check if item is in cache already
 
   if (cachedResponse) {
-    const cachedItem = await cachedResponse.json();
-    return cachedItem;
+    try {
+      const cachedText = await cachedResponse.text();
+      const cachedItem = cachedText ? JSON.parse(cachedText) : null;
+      return cachedItem;
+    } catch (e) {
+      return null;
+    }
   }
 
   return null;
 }
 
 export async function cacheResponse(
-  c: Context<{}, any, {}>,
   cache: Cache,
   categoryId: string,
   itemId: string,
   data: any,
   cacheTTL: number = 60 * 60 // 1 hour
 ): Promise<void> {
-  const cacheKey = `${c.req.url}::${categoryId}-${itemId}`;
-  const cacheResponse = new Response(JSON.stringify(data.user));
-  cacheResponse.headers.set("Cache-Control", `max-age=${cacheTTL}`);
-  await cache.put(cacheKey, cacheResponse);
+  try {
+    const cacheKey = `${QUALIFIED_CACHE_URL}::${categoryId}-${itemId}`;
+    const cacheResponse = new Response(JSON.stringify(data));
+    cacheResponse.headers.set("Cache-Control", `max-age=${cacheTTL}`);
+    await cache.put(cacheKey, cacheResponse);
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+export async function deleteCachedResponse(
+  cache: Cache,
+  categoryId: string,
+  itemId: string
+): Promise<void> {
+  try {
+    const cacheKey = `${QUALIFIED_CACHE_URL}::${categoryId}-${itemId}`;
+    await cache.delete(cacheKey);
+  } catch (e) {
+    console.error(e);
+  }
 }
